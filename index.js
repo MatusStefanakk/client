224365a4ce5169bf4dd2852554b55c56f4b5c0e4
Matúš Štefaňák <>
2024-09-06 22:08:43 +0200

    test

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ app.use(bodyParser.json());
 
 // PostgreSQL connection configuration using environment variables
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-});
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    ssl: {
+      rejectUnauthorized: false, // Required for Supabase connections
+    },
+  });
+  
 
 // Serve the main page
 app.get('/', (request, response) => {
